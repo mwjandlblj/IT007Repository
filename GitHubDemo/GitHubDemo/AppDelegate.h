950bd752e7e737ec94412ea1ebb6fb9e0854b75a
mwjandlblj <student@maweijie.local>
2016-06-22 10:13:39 +0800

    mwj
    add project

---
+++ GitHubDemo/GitHubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubDemo
+//
+//  Created by student on 16/6/22.
+//  Copyright © 2016年 Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubDemo/GitHubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubDemo
+//
+//  Created by student on 16/6/22.
+//  Copyright © 2016年 Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
